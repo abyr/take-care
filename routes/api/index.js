043a3bb6f7edf6f4fb2d85ba4a28e960bf0586c2
Aleksandr Denisenko <abyrcheg@gmail.com>
2014-04-24 23:17:28 +0300

    find and count

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     router = express.Router(),
-    ErrorLog = require("../../models/error").Error;
+    ErrorLog = require("../../models/error").Error,
+    limit = 10;
 
 router.post('/', function(req, res) {
     var message = req.body.message,
@@ -30,7 +31,36 @@ router.post('/', function(req, res) {
 });
 
 router.get('/', function(req, res) {
-    res.send(501);
+    // available filters
+    // passed filters
+    // defaults
+    var filters = {};
+
+    ErrorLog.find(filters, function(err, data) {
+        if (err) {
+            return res.send(500, {
+                error: true,
+                message: err.message
+            });
+        }
+        res.send(200, data);
+    });
+});
+
+router.get('/count', function(req, res) {
+    var filters = {};
+
+    ErrorLog.collection.count(filters, function (err, count) {
+        if (err) {
+            return res.send(500, {
+                error: true,
+                message: err.message
+            });
+        }
+        res.send(200, {
+            count: count
+        });
+    });
 });
 
 module.exports = router;
