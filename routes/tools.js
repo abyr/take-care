d2468d1f2d15b348f8c46a6aa338aeb030b90427
Aleksandr Denisenko <abyrcheg@gmail.com>
2014-06-12 01:59:19 +0300

    tools router and template

---
+++ routes/tools.js
@@ -0,0 +1,31 @@
+/**
+ * System tools
+ */
+
+var express = require('express'),
+    _ = require('lodash'),
+    router = express.Router(),
+    ErrorLog = require("../models/error").ErrorLog;
+
+router.get('/', function(req, res, next) {
+    var period = req.body.period || req.query.period || false;
+
+    if (!period) {
+        res.render('tools', {
+            title: 'Tools',
+            periods: ['year', 'month']
+        });
+    } else {
+        ErrorLog.removeLaterThen(period, function(err) {
+            if (err) {
+                return next(err);
+            }
+            res.render('tools', {
+                title: 'Tools',
+                period: period
+            });
+        });
+    }
+});
+
+module.exports = router;
