148b520803d9270ea7d0e9a2e153971a31d556e7
Aleksandr Denisenko <abyrcheg@gmail.com>
2014-06-21 22:28:46 +0300

    partials

---
+++ routes/partials.js
@@ -0,0 +1,15 @@
+/**
+ * List of partials
+ */
+
+var partials = {
+    head: 'blocks/head',
+    footer: 'blocks/footer',
+    error: 'blocks/error',
+    pagination: 'blocks/pagination',
+    'pagination-script': 'blocks/pagination-script',
+    filters: 'blocks/filters',
+    menu: 'blocks/menu'
+};
+
+module.exports = partials;
